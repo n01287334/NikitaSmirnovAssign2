11fd011f92e954a3432ddf763c3eb1c5645b7494
Nikita Smirnov <n01287334@humbermail.ca>
2020-11-10 21:41:29 -0500

    stuck on list view

--- nikitalab2/src/main/java/nikita/smirnov/n01287334/smirnovActivitySecond.java
+++ nikitalab2/src/main/java/nikita/smirnov/n01287334/smirnovActivitySecond.java
@@ -63,6 +63,20 @@ public class smirnovActivitySecond extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
- 
+    private class MyAdapter extends BaseAdapter {
+
+        // override other abstract methods here
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup container) {
+            if (convertView == null) {
+                convertView = getLayoutInflater().inflate(R.layout.list_item, container, false);
+            }
+
+            ((TextView) convertView.findViewById(android.R.id.text1))
+                    .setText(getItem(position));
+            return convertView;
+        }
+    }
 
 }
